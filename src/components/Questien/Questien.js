011e676dcc87a943bc875466412098ae6e1de6cc
Bijoy <hskbijoy123@gmail.com>
2022-10-12 22:37:46 +0600

    finish

--- src/components/Questien/Questien.js
+++ src/components/Questien/Questien.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './Questien.css'
 const Questien = (props) => {
-    // const { question, options, correctAnswer } = props;
-    // const { question } = props;
+
     const { question, options, correctAnswer } = props.question
-    console.log(question)
     return (
         <div className='topic'>
             <h2>{question}</h2>
@@ -15,6 +13,7 @@ const Questien = (props) => {
                     options.map(element => { return <h5 > {element}</h5> })
 
                 }
+
             </div>
 
         </div>
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Topics = () => {
     return (
         <div>
-            <h3>its Topics</h3>
+
         </div>
     );
 };
